01844eeca9429e7a6c45dac4eae4f0fa303d133a
nemrac99 <nemrac99@hotmail.com>
2025-02-04 15:16:20 -0600

    updated env shite

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
-;
+const stripe = require("stripe")("pk_test_51Qos0bGhpdCpCvFScxcUFH40bZk2LMSyZtbunScBjIbQexJOPhupEd8V7jdmvgcF3qKngxlT6F616lmgXIJFEUmz00Qwyipzfy");
+//const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
   try {
@@ -9,19 +9,19 @@ exports.handler = async (event) => {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
-      payment_method_types: ["card"],
+      payment_method_types: ["card"]
     });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ paymentIntent }),
+      body: JSON.stringify({ paymentIntent })
     };
   } catch (error) {
-    console.log({ error });
+      console.log({ error });
 
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error }),
-    };
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error })
+      };
   }
 };
